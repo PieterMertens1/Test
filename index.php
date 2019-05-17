f746a479ca6de63b6b172e223595c35b5a2fc7ac
u134153 <pieter.mertens@crelan.be>
2019-05-17 17:15:50 +0200

    test

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <body>
 
 <div class="container" id="main-content">
-	<h2>Welcome to my website!</h2>
+	<h2>Welcome to myCrelan!</h2>
 	<p>Some content goes here! Let's go with the classic "lorem ipsum."</p>
 
 	<p>
@@ -15,6 +15,11 @@
 	<p>
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 	</p>
+	
+	<p>
+		Here is an image of free found on the internet
+	</p>
+	<img src="https://leuvenbuitenlandreisdotcom.files.wordpress.com/2012/06/img_23962.jpg"/>
 </div>
 
 
