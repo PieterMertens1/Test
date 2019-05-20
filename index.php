fdf73e01a44266be46332a333e669c1adfa2e19f
u134153 <pieter.mertens@crelan.be>
2019-05-20 09:35:08 +0200

    test

--- index.php
+++ index.php
@@ -1,23 +1,30 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-
+  <title>Bootstrap Theme Company</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <style>
+	.jumbotron { 
+	background-color: #7cfc00; /* Orange */
+	color: #ffffff;
+}
+</style>
 </head>
 <body>
 
 <div class="container" id="main-content">
-	<h2>Welcome to myCrelan!</h2>
-	<p>Some content goes here! Let's go with the classic "lorem ipsum."</p>
+
+<div class="jumbotron text-center">
+  <h1>Welcome to myCrelan!</h1> 
+  <p>Landbouwkrediet en andere bankdingen enzo</p> 
+</div>
 
 	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	</p>
-	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	</p>
-	
-	<p>
-		Here is an image of free found on the internet
+		Here is an image of Free found on the internet,
 	</p>
 	<img src="https://leuvenbuitenlandreisdotcom.files.wordpress.com/2012/06/img_23962.jpg" width="300px"/>
 </div>
